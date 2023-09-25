4265feeecddbfbff9da6e3fd024539fb4471e14b
gzero1016 <gzero1016@gmail.com>
2023-09-25 16:17:59 +0900

    feat: login 완료

--- back/src/main/java/com/toy_project/instagram/dto/SignupReqDto.java
+++ back/src/main/java/com/toy_project/instagram/dto/SignupReqDto.java
@@ -9,13 +9,13 @@ import javax.validation.constraints.Pattern;
 @Data
 public class SignupReqDto {
     // 회원가입할때 들어올 데이터들 (클라이언트가 전달해준 변수 그대로)
-    @Pattern(regexp = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]+|[\\d]{11}+$", message = "이메일 또는 전화번호를 입력하세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]*$|^[0-9]{11}$", message = "이메일 또는 전화번호를 입력하세요.")
     private String phoneAndEmail;
-    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]*$", message = "이름은 한글로만 입력해주세요.")
+    @Pattern(regexp = "^[가-힣]*$", message = "이름은 한글만 입력할 수 있습니다.")
     private String name;
-    @Pattern(regexp = "", message = "사용자이름은 공백일 수 없습니다.")
+    @Pattern(regexp = "^(?=.*[a-z])[a-z0-9_.]*$", message = "사용할 수 없는 사용자 이름입니다. 다른 이름을 사용하세요.")
     private String username;
-    @Pattern(regexp = "", message = "비밀번호는 공백일 수 없습니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$", message = "비밀번호는 영문, 숫자 조합으로 8자 이상 입력하세요.")
     private String password;
 
     // dto를 entity로 변환하는 과정
